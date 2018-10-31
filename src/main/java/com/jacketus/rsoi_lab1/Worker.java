b22fd50a1203ff81a39c7bb78b9cc04d0cbfd435
Oleg <jktu2308@gmail.com>
2018-10-31 19:18:44 +0300

    Fix tests, add stages in yml

--- src/main/java/com/jacketus/rsoi_lab1/Worker.java
+++ src/main/java/com/jacketus/rsoi_lab1/Worker.java
@@ -1,4 +1,5 @@
 package com.jacketus.rsoi_lab1;
+import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
@@ -6,13 +7,19 @@ public class Worker {
     public static String getSHA256Hash(String s) {
         String encryptedString = "No string";
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-            messageDigest.update(s.getBytes(StandardCharsets.UTF_8));
-            encryptedString = new String(messageDigest.digest());
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hash = digest.digest(s.getBytes("UTF-8"));
+            encryptedString = bytesToHex(hash);
         }
         catch (Exception e) {
             e.printStackTrace();
         }
         return encryptedString;
     }
+
+    private static String bytesToHex(byte[] hash) {
+        return DatatypeConverter.printHexBinary(hash);
+    }
+
 }
--- src/test/java/com/jacketus/rsoi_lab1/ApplicationTest.java
+++ src/test/java/com/jacketus/rsoi_lab1/ApplicationTest.java
@@ -2,22 +2,17 @@ package com.jacketus.rsoi_lab1;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
 public class ApplicationTest {
-    @Autowired
 
     @Test
     public void testHashing() {
         assertEquals("Error!", Worker.getSHA256Hash("NULL"), "FB329000228CC5A24C264C57139DE8BF854FC86FC18BF1C04AB61A2B5CB4B921");
-        assertEquals("Error!", Worker.getSHA256Hash(""), "No string");
+        assertEquals("Error!", Worker.getSHA256Hash(""), "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855");
+        assertEquals("Error!", Worker.getSHA256Hash("some_string"), "539A374FF43DCE2E894FD4061AA545E6F7F5972D40EE9A1676901FB92125FFEE");
     }
 }
