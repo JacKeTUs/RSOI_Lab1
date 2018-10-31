076a99fe9be4507adf6a4c003905abdd9cca8ed4
Oleg <jktu2308@gmail.com>
2018-10-31 16:07:48 +0300

    Fix Controller

--- src/main/java/com/jacketus/rsoi_lab1/MainController.java
+++ src/main/java/com/jacketus/rsoi_lab1/MainController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/hello")
 public class MainController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String index(ModelMap model) {
+    public String helloPage(ModelMap model) {
         model.addAttribute("my_body", "Hello world!");
         //model.addAttribute("my_title", "Hello!");
         return "hello";
